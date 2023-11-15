6b0958ab615c45a73490434d2b7b6f475f8ae2f2
parkgunwook1 <pkw85428@naver.com>
2023-11-15 23:26:36 +0900

    project

---
+++ project_final/front/assets/js/main.js
@@ -0,0 +1,17 @@
+import { bs } from './modules/bsComponents.js'
+import { shadow } from './modules/shadow.js'
+import { wowAnimate } from './modules/wowAnimate.js'
+import { scroll } from './modules/scroll.js'
+
+bs()
+if (document.querySelector('#shadow-box')) {
+  shadow()
+}
+
+if (document.querySelector('.wow')) {
+  wowAnimate()
+}
+
+if (document.querySelector('#scrolltoTop')) {
+  scroll()
+}
